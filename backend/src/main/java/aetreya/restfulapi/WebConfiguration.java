9a84d976d393412fb9080b386929118c503b2cb7
galangramadan <galangynr@gmail.com>
2023-10-06 03:06:25 +0700

    feat: add cors config

--- backend/src/main/java/aetreya/restfulapi/WebConfiguration.java
+++ backend/src/main/java/aetreya/restfulapi/WebConfiguration.java
@@ -4,6 +4,7 @@ import aetreya.restfulapi.resolver.UserArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,4 +20,13 @@ public class WebConfiguration implements WebMvcConfigurer {
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
         resolvers.add(userArgumentResolver);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowCredentials(true);
+    }
+
 }
